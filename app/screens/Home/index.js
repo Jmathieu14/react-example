42f16884ac2d4f6ebd2e57545117e8505c4adb78
Jacques Mathieu <jmathfb@gmail.com>
2019-03-17 20:40:59 -0400

    array to component render implementation

--- app/screens/Home/index.js
+++ app/screens/Home/index.js
@@ -1,29 +1,69 @@
 import React, {Component} from 'react';
 
+// Folder component
+class Folder extends React.Component {
+    render() {
+        return(
+          <div className="folder">
+            <div className="folder-title">
+              {this.props.name}
+            </div>
+          </div>
+        );
+    }
+}
+
+// The folder list component
+class FolderList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.tags = this.props.tags;
+  }
+    render() {
+        // Map each folder name to a folder
+        const folders = this.props.tags.map((tag) => 
+          <Folder name={tag} />);
+        return(
+          <div className="folderlist">
+            <div className="fl-title">Folder List</div>
+            <ul>
+              {folders}
+            </ul>
+          </div>                                
+        );
+    }
+}
+
+
+// The tabber component
+class Tabber extends React.Component {
+ constructor(props) {
+    super(props);
+    this.tags = this.props.tags;
+  }
+    render() {
+        return(
+          <div className="tabber">
+            <div className="tabber-title"> {this.props.name}
+            </div>
+            <FolderList tags={this.props.tags} />
+          </div>
+        );
+    }
+}
+
+const my_folders = ["f1", "f2", "f3"];
+
+// The main page
 export default class Home extends Component {
 
   render() {
+    console.log(my_folders);
     return (
       <section className="container home">
-        <form
-          className="form-inline"
-          role="form"
-          onSubmit={this.handleSubmit}
-        >
-          <div className="form-group">
-            <div className="input-group">
-              <input
-                type="text"
-                placeholder="Enter a GitHub user..."
-                className="form-control"
-                ref={ref => (this._input = ref)}
-              />
-            </div>
-          </div>
-          <button type="submit" className="btn btn-primary">
-            Go
-          </button>
-        </form>
+        <Tabber
+          name="Ye Tabber" tags={my_folders}
+        />
       </section>
     );
   }
